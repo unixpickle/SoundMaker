341bac6dd1d821be7d84e65616c47d3a7a7718a0
Alex Nichol <alexnichol@comcast.net>
2012-02-20 20:51:45 -0500

    Initial Commit

---
+++ SoundMaker/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  SoundMaker
+//
+//  Created by Alex Nichol on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
